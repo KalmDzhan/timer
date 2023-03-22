df639f3587233fffdbc0cc83b619217ce0cd9f56
DzhangrErdniev21D <85980107+KalmDzhan@users.noreply.github.com>
2023-03-22 17:32:27 +0300

    Add files via upload

---
+++ TimerApp/TimerApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using TimerApp;
+
+Console.Write("Введите минуты: ");
+int minutes = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите секунды: ");
+int seconds = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+
+Timer1 time = new(minutes, seconds);
+
+time.Run();
---
+++ TimerApp/TimerApp/Timer1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+
+namespace TimerApp
+{
+    public class Timer1
+    {
+        public int Minute { get; set; }
+        public int Second { get; set; }
+
+        public delegate void TimerTickEventHandler(object sender, TimerTickEventArgs e);
+        public event TimerTickEventHandler TimerTick;
+
+        public delegate void TimerCompletedEventHandler(object sender, EventArgs e);
+        public event TimerCompletedEventHandler TimerCompleted;
+
+        public Timer1(int minute, int second)
+        {
+            Minute = minute;
+            Second = second;
+        }
+
+        public async void Run()
+        {
+            while (true)
+            {
+                if (Second != 0 || Minute != 0)
+                {
+                    TimerTick?.Invoke(this, new TimerTickEventArgs(Minute, Second));
+                    Console.WriteLine("Минуты: " + Minute + " Секунды: " + Second);
+                    Second--;
+                    Thread.Sleep(1000);
+                    Console.Clear();
+
+                    if (Second < 0)
+                    {
+                        Second = 59;
+                        Minute--;
+                    }
+                }
+                else
+                {
+                    TimerCompleted?.Invoke(this, EventArgs.Empty);
+                    Console.WriteLine("Время вышло");
+                    break;
+                }
+            }
+        }
+    }
+
+    public class TimerTickEventArgs : EventArgs
+    {
+        public int Minute { get; set; }
+        public int Second { get; set; }
+
+        public TimerTickEventArgs(int minute, int second)
+        {
+            Minute = minute;
+            Second = second;
+        }
+    }
+}
